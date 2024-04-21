1f23819aeee46d4a3f10b92996feb0b188086f32
Jes015 <jes.18.sin.15@gmail.com>
2024-04-20 19:04:33 -0500

    feat: add sign out button (#38)
    close #38

--- src/components/feat/AuthModal/hooks/useAuthModal.ts
+++ src/components/feat/AuthModal/hooks/useAuthModal.ts
@@ -4,18 +4,21 @@ import { useEffect, useState } from "react"
 
 export const useAuthModal = () => {
     const [modalOpen, setModalOpen] = useState(false)
-
+    const [isAuth, setIsAuth] = useState(false)
 
     useEffect(() => {
         const supabase = createClient()
 
         const { data } = supabase.auth.onAuthStateChange((event, session) => {
 
+            console.log({ event, session})
             if (event === 'SIGNED_IN') {
+                toggleIsAuth(true)
                 toggleModalStatus(false)
             } else if (event === 'SIGNED_OUT') {
                 toggleModalStatus(true)
-            } else if (event === 'INITIAL_SESSION' && session == null){
+                toggleIsAuth(false)
+            } else if (event === 'INITIAL_SESSION' && session == null) {
                 toggleModalStatus(true)
             }
         })
@@ -41,5 +44,9 @@ export const useAuthModal = () => {
         setModalOpen(prev => newValue ?? !prev)
     }
 
-    return { modalOpen, toggleModalStatus }
+    const toggleIsAuth = (newValue?: boolean) => {
+        setIsAuth(prev => newValue ?? !prev)
+    }
+
+    return { isAuth, toggleIsAuth, modalOpen, toggleModalStatus }
 }
